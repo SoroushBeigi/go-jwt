8505f96d192dc464ec3422fba6084c1ce6f8ebc0
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-05 11:54:54 +0330

    replacing abort request with proper error messages

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -17,7 +17,7 @@ func RequireAuth(c *gin.Context) {
 	fmt.Println("In middleware!")
 	providedToken := c.Request.Header.Get("Authorization")
 	if providedToken == "" {
-		c.AbortWithStatus(http.StatusUnauthorized)
+		c.JSON(http.StatusUnauthorized,gin.H{"Unauthorized:":"Please provide a token"})
 	}
 
 	token, err := jwt.Parse(providedToken, func(token *jwt.Token) (interface{}, error) {
@@ -35,17 +35,17 @@ func RequireAuth(c *gin.Context) {
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			c.JSON(http.StatusUnauthorized,gin.H{"Unauthorized:":"Token Expired"})
 		}
 		var user models.User
 		initializers.DB.First(&user, claims["sub"])
 		if user.ID == 0 {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			c.JSON(http.StatusUnauthorized,gin.H{"Unauthorized:":"User not found"})
 		}
 		c.Set("user", user)
 		c.Next()
 	} else {
-		c.AbortWithStatus(http.StatusUnauthorized)
+		c.JSON(http.StatusUnauthorized,gin.H{"Unauthorized:":"Token invalid"})
 	}
 
 }
